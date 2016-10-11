9c32c1935415ab01022e61c9f4a2d9d2c04abfdf
Zetra <zetra@mail.ugm.ac.id>
2016-10-11 12:24:48 +0700

    Add Qiscus class documentation

--- qiscus-sdk/src/main/java/com/qiscus/sdk/Qiscus.java
+++ qiscus-sdk/src/main/java/com/qiscus/sdk/Qiscus.java
@@ -45,14 +45,11 @@ import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
 /**
- * Created on : August 18, 2016
- * Author     : zetbaitsu
- * Name       : Zetra
- * Email      : zetra@mail.ugm.ac.id
- * GitHub     : https://github.com/zetbaitsu
- * LinkedIn   : https://id.linkedin.com/in/zetbaitsu
+ * The main class of Qiscus SDK. Init qiscus engine sdk, set qiscus user, start the chatting and all
+ * about qiscus configuration can be accessed from here.
  */
 public class Qiscus {
+
     private static Application APP_INSTANCE;
     private static volatile Handler APP_HANDLER;
     private static LocalDataManager LOCAL_DATA_MANAGER;
@@ -61,6 +58,27 @@ public class Qiscus {
     private static String APP_ID;
     private static long HEART_BEAT;
 
+    private Qiscus() {
+    }
+
+    /**
+     * The first method you need to be invoke to using qiscus sdk. Call this method from your Application
+     * class. You can not using another qiscus feature if you not invoke this method first. Here sample
+     * to call this method:
+     * <pre>
+     * {@code
+     * public class SampleApps extends Application {
+     *  public void onCreate() {
+     *      super.onCreate();
+     *      Qiscus.init(this, "yourQiscusAppId");
+     *  }
+     * }
+     * }
+     * </pre>
+     *
+     * @param application Application instance
+     * @param qiscusAppId Your qiscus application Id
+     */
     public static void init(Application application, String qiscusAppId) {
         APP_INSTANCE = application;
         APP_ID = qiscusAppId;
@@ -72,69 +90,145 @@ public class Qiscus {
         APP_INSTANCE.startService(new Intent(APP_INSTANCE, QiscusPusherService.class));
     }
 
+    /**
+     * Use this method set qiscus user. If user doesn't exist at your qiscus engine, He/She will be
+     * registered automatically.
+     *
+     * @param userEmail The email or username of qiscus user
+     * @param userKey   Qiscus user key
+     * @return User builder
+     */
     public static SetUserBuilder setUser(String userEmail, String userKey) {
         return new SetUserBuilder(userEmail, userKey);
     }
 
+    /**
+     * Use this method if we need application context instance
+     *
+     * @return Your application instance
+     */
     public static Application getApps() {
         checkAppIdSetup();
         return APP_INSTANCE;
     }
 
+    /**
+     * Will return the exact apps name
+     *
+     * @return The apps name.
+     */
     public static String getAppsName() {
         checkAppIdSetup();
         return APP_INSTANCE.getApplicationInfo().loadLabel(APP_INSTANCE.getPackageManager()).toString();
     }
 
+    /**
+     * Needed to run something at main thread handler
+     *
+     * @return Main thread handler
+     */
     public static Handler getAppsHandler() {
         checkAppIdSetup();
         return APP_HANDLER;
     }
 
+    /**
+     * Accessor to get current qiscus app id
+     *
+     * @return Current qiscus app id
+     */
     public static String getAppId() {
         checkAppIdSetup();
         return APP_ID;
     }
 
+    /**
+     * For checking is qiscus user has been setup
+     *
+     * @return true if already setup, false if not yet
+     */
     public static boolean hasSetupUser() {
         return LOCAL_DATA_MANAGER.isLogged();
     }
 
+    /**
+     * Accessor to get current qiscus user account
+     *
+     * @return Current qiscus user account
+     */
     public static QiscusAccount getQiscusAccount() {
         checkUserSetup();
         return LOCAL_DATA_MANAGER.getAccountInfo();
     }
 
+    /**
+     * Accessor to get current qiscus user token
+     *
+     * @return Current qiscus user token
+     */
     public static String getToken() {
         checkUserSetup();
         return LOCAL_DATA_MANAGER.getToken();
     }
 
+    /**
+     * Use this method to get current qiscus chatting configuration, you can also modify it.
+     *
+     * @return Current qiscus chatting configuration
+     */
     public static QiscusChatConfig getChatConfig() {
         checkAppIdSetup();
         return CHAT_CONFIG;
     }
 
+    /**
+     * Use this method to open a chatting room with other user.
+     *
+     * @param email Email or username of other user to chat.
+     * @return Chat room builder
+     */
     public static ChatBuilder buildChatRoomWith(String email) {
         checkUserSetup();
         return new ChatBuilder(email);
     }
 
+    /**
+     * Use this method to start an Activity for chatting with other user.
+     *
+     * @param email Email or username of other user to chat.
+     * @return Chatting Activity builder
+     */
     public static ChatActivityBuilder buildChatWith(String email) {
         checkUserSetup();
         return new ChatActivityBuilder(email);
     }
 
+    /**
+     * Use this method if you want to generate a Fragment for chatting with other user.
+     *
+     * @param email Email or username of other user to chat.
+     * @return Chatting Fragment builder
+     */
     public static ChatFragmentBuilder buildChatFragmentWith(String email) {
         checkUserSetup();
         return new ChatFragmentBuilder(email);
     }
 
+    /**
+     * Set the heartbeat of qiscus synchronization chat data. Default value is 500ms
+     *
+     * @param heartBeat Heartbeat duration in milliseconds
+     */
     public static void setHeartBeat(long heartBeat) {
         checkAppIdSetup();
         HEART_BEAT = heartBeat;
     }
 
+    /**
+     * Get the current qiscus heartbeat duration
+     *
+     * @return Heartbeat duration in milliseconds
+     */
     public static long getHeartBeat() {
         return HEART_BEAT;
     }
@@ -152,6 +246,9 @@ public class Qiscus {
         }
     }
 
+    /**
+     * Clear all current user qiscus data, you can call this method when user logout for example.
+     */
     public static void clearUser() {
         LOCAL_DATA_MANAGER.clearData();
         QiscusDataBaseHelper.getInstance().clear();
@@ -208,22 +305,45 @@ public class Qiscus {
             this.password = password;
         }
 
+        /**
+         * Set the qiscus user name, if you not call this method, the username will be the same with
+         * qiscus user email
+         *
+         * @param username The name
+         * @return builder
+         */
         public SetUserBuilder withUsername(String username) {
             this.username = username;
             return this;
         }
 
+        /**
+         * Set the avatar url for the qiscus user.
+         *
+         * @param avatarUrl The string url of avatar
+         * @return builder
+         */
         public SetUserBuilder withAvatarUrl(String avatarUrl) {
             this.avatarUrl = avatarUrl;
             return this;
         }
 
+        /**
+         * Submit to qiscus engine and save the user account
+         *
+         * @param listener Listener of saving user process
+         */
         public void save(SetUserListener listener) {
             save().subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(listener::onSuccess, listener::onError);
         }
 
+        /**
+         * Submit to qiscus engine and save the user account with RxJava style.
+         *
+         * @return Observable of Qiscus account
+         */
         public Observable<QiscusAccount> save() {
             return QiscusApi.getInstance()
                     .loginOrRegister(email, password, username, avatarUrl)
@@ -235,8 +355,18 @@ public class Qiscus {
     }
 
     public interface SetUserListener {
+        /**
+         * Called if saving user succeed
+         *
+         * @param qiscusAccount Saved qiscus account
+         */
         void onSuccess(QiscusAccount qiscusAccount);
 
+        /**
+         * Called if error happened while saving qiscus user account. e.g network error
+         *
+         * @param throwable The cause of error
+         */
         void onError(Throwable throwable);
     }
 
@@ -248,18 +378,33 @@ public class Qiscus {
             emails.add(email);
         }
 
+        /**
+         * Add more qiscus user email to the chat room. For group chat.
+         *
+         * @param email Qiscus user email
+         * @return builder
+         */
         public ChatBuilder addEmail(String email) {
             emails.add(email);
             return this;
         }
 
-
+        /**
+         * Build the chat room
+         *
+         * @param listener Listener of building chat room process
+         */
         public void build(ChatBuilderListener listener) {
             build().subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(listener::onSuccess, listener::onError);
         }
 
+        /**
+         * Build the chat room as Observable
+         *
+         * @return Observable chat room
+         */
         public Observable<QiscusChatRoom> build() {
             return QiscusApi.getInstance()
                     .getChatRoom(new ArrayList<>(emails));
@@ -267,8 +412,18 @@ public class Qiscus {
     }
 
     public interface ChatBuilderListener {
+        /**
+         * Called if building chat room succeed
+         *
+         * @param qiscusChatRoom Built chat room
+         */
         void onSuccess(QiscusChatRoom qiscusChatRoom);
 
+        /**
+         * Called if error happened while building chat room. e.g network error
+         *
+         * @param throwable The cause of error
+         */
         void onError(Throwable throwable);
     }
 
@@ -284,27 +439,57 @@ public class Qiscus {
             emails.add(email);
         }
 
+        /**
+         * Add more qiscus user email to the chat room. For group chat.
+         *
+         * @param email Qiscus user email
+         * @return builder
+         */
         public ChatActivityBuilder addEmail(String email) {
             emails.add(email);
             return this;
         }
 
+        /**
+         * Set the title of of chat activity. Default id "Chat"
+         *
+         * @param title The title of chat room
+         * @return builder
+         */
         public ChatActivityBuilder withTitle(String title) {
             this.title = title;
             return this;
         }
 
+        /**
+         * Set the subtitle of chat activity
+         *
+         * @param subtitle The subtitle of chat
+         * @return builder
+         */
         public ChatActivityBuilder withSubtitle(String subtitle) {
             this.subtitle = subtitle;
             return this;
         }
 
+        /**
+         * Build the Chat activity intent
+         *
+         * @param context  Context for start the Activity
+         * @param listener Listener of building chat activity
+         */
         public void build(Context context, ChatActivityBuilderListener listener) {
             build(context).subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(listener::onSuccess, listener::onError);
         }
 
+        /**
+         * Build the chat activity as observable
+         *
+         * @param context Context to start the activity
+         * @return Observable of Intent for start chat activity
+         */
         public Observable<Intent> build(Context context) {
             return QiscusApi.getInstance()
                     .getChatRoom(new ArrayList<>(emails))
@@ -317,8 +502,18 @@ public class Qiscus {
     }
 
     public interface ChatActivityBuilderListener {
+        /**
+         * Called if building Chat Activity succeed
+         *
+         * @param intent Intent for start chat activity
+         */
         void onSuccess(Intent intent);
 
+        /**
+         * Called if error happened while building chat activity. e.g network error
+         *
+         * @param throwable The cause of error
+         */
         void onError(Throwable throwable);
     }
 
@@ -330,17 +525,33 @@ public class Qiscus {
             emails.add(email);
         }
 
+        /**
+         * Add more qiscus user email to the chat room. For group chat.
+         *
+         * @param email Qiscus user email
+         * @return builder
+         */
         public ChatFragmentBuilder addEmail(String email) {
             emails.add(email);
             return this;
         }
 
+        /**
+         * Build the Chat fragment instance
+         *
+         * @param listener Listener of building chat fragment
+         */
         public void build(ChatFragmentBuilderListener listener) {
             build().subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(listener::onSuccess, listener::onError);
         }
 
+        /**
+         * Build the chat fragment as observable
+         *
+         * @return Observable of chat fragment
+         */
         public Observable<QiscusChatFragment> build() {
             return QiscusApi.getInstance()
                     .getChatRoom(new ArrayList<>(emails))
@@ -349,8 +560,18 @@ public class Qiscus {
     }
 
     public interface ChatFragmentBuilderListener {
+        /**
+         * Called if building Chat Fragment succeed
+         *
+         * @param qiscusChatFragment Chat Fragment instance
+         */
         void onSuccess(QiscusChatFragment qiscusChatFragment);
 
+        /**
+         * Called if error happened while building chat fragment. e.g network error
+         *
+         * @param throwable The cause of error
+         */
         void onError(Throwable throwable);
     }
 }
