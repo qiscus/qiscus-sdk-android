52383c2e8a5b5116e9188a1ca7843cacdccb8b1e
Zetra <zetra@mail.ugm.ac.id>
2016-11-25 12:32:49 +0700

    Add click handler

--- qiscus-sdk/src/main/java/com/qiscus/sdk/ui/fragment/QiscusGroupDetailFragment.java
+++ qiscus-sdk/src/main/java/com/qiscus/sdk/ui/fragment/QiscusGroupDetailFragment.java
@@ -16,6 +16,8 @@
 
 package com.qiscus.sdk.ui.fragment;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -23,15 +25,28 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.qiscus.sdk.R;
 import com.qiscus.sdk.data.model.QiscusChatRoom;
 import com.trello.rxlifecycle.components.support.RxFragment;
 
 public class QiscusGroupDetailFragment extends RxFragment {
     protected static final String CHAT_ROOM_DATA = "chat_room_data";
+    protected static final int PICK_IMAGE_REQUEST = 2;
 
-    private ViewGroup memberContainer;
+    protected CollapsingToolbarLayout collapsingToolbar;
+    protected ImageView groupPicture;
+    protected ImageView buttonEditGroupPicture;
+    protected TextView groupName;
+    protected ImageView buttonEditGroupName;
+    protected TextView membersTitle;
+    protected ImageView buttonAddMember;
+    protected ViewGroup memberContainer;
+    protected View buttonLeaveGroup;
 
     protected QiscusChatRoom qiscusChatRoom;
 
@@ -56,12 +71,22 @@ public class QiscusGroupDetailFragment extends RxFragment {
     }
 
     protected void onLoadView(View view) {
-        CollapsingToolbarLayout collapsingToolbar = (CollapsingToolbarLayout) view.findViewById(R.id.collapsing_toolbar);
+        collapsingToolbar = (CollapsingToolbarLayout) view.findViewById(R.id.collapsing_toolbar);
         collapsingToolbar.setCollapsedTitleTextColor(Color.WHITE);
         collapsingToolbar.setExpandedTitleColor(Color.WHITE);
-        collapsingToolbar.setTitle("Senam Bugar");
-
+        groupPicture = (ImageView) view.findViewById(R.id.group_photo);
+        buttonEditGroupPicture = (ImageView) view.findViewById(R.id.button_edit_picture);
+        groupName = (TextView) view.findViewById(R.id.group_name);
+        buttonEditGroupName = (ImageView) view.findViewById(R.id.button_edit_group_name);
+        membersTitle = (TextView) view.findViewById(R.id.member_title);
+        buttonAddMember = (ImageView) view.findViewById(R.id.button_add_member);
         memberContainer = (ViewGroup) view.findViewById(R.id.member_container);
+        buttonLeaveGroup = view.findViewById(R.id.button_leave_group);
+
+        buttonEditGroupPicture.setOnClickListener(v -> pickGroupPicture());
+        buttonEditGroupName.setOnClickListener(v -> editGroupName());
+        buttonAddMember.setOnClickListener(v -> addNewMember());
+        buttonLeaveGroup.setOnClickListener(v -> leaveGroup());
     }
 
     @Override
@@ -72,7 +97,20 @@ public class QiscusGroupDetailFragment extends RxFragment {
 
     protected void onCreateGroupDetailComponents(Bundle savedInstanceState) {
         resolveChatRoom(savedInstanceState);
-        for (int i = 0; i < 30; i++) {
+
+        collapsingToolbar.setTitle(qiscusChatRoom.getName());
+        groupName.setText(qiscusChatRoom.getName());
+
+        Glide.with(this)
+                .load(qiscusChatRoom.getLastCommentMessage())
+                .dontAnimate()
+                .placeholder(R.drawable.ic_qiscus_avatar)
+                .error(R.drawable.ic_qiscus_avatar)
+                .into(groupPicture);
+
+        membersTitle.setText("Members (" + qiscusChatRoom.getMember().size() + ")");
+
+        for (String s : qiscusChatRoom.getMember()) {
             View item = LayoutInflater.from(getActivity()).inflate(R.layout.item_qiscus_group_member, null);
             memberContainer.addView(item);
         }
@@ -88,5 +126,46 @@ public class QiscusGroupDetailFragment extends RxFragment {
             getActivity().finish();
             return;
         }
+    }
+
+    protected void pickGroupPicture() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    protected void editGroupName() {
+        Toast.makeText(getActivity(), "Edit", Toast.LENGTH_SHORT).show();
+    }
+
+    private void addNewMember() {
+        Toast.makeText(getActivity(), "Add", Toast.LENGTH_SHORT).show();
+    }
+
+    protected void leaveGroup() {
+        Toast.makeText(getActivity(), "Leave", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                //showError(getString(R.string.chat_error_failed_open_picture));
+                return;
+            }
+            /*try {
+                qiscusChatPresenter.sendFile(QiscusFileUtil.from(data.getData()));
+            } catch (IOException e) {
+                showError(getString(R.string.chat_error_failed_read_picture));
+                e.printStackTrace();
+            }*/
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putParcelable(CHAT_ROOM_DATA, qiscusChatRoom);
     }
 }
