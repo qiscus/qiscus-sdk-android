4b0c6b7e37ff69701b80fa53ac87dc84323cc19d
Zetra <zetra@mail.ugm.ac.id>
2016-12-13 17:56:03 +0700

    Clear image also

--- qiscus-sdk/src/main/java/com/qiscus/sdk/ui/view/QiscusLinkPreviewView.java
+++ qiscus-sdk/src/main/java/com/qiscus/sdk/ui/view/QiscusLinkPreviewView.java
@@ -100,6 +100,8 @@ public class QiscusLinkPreviewView extends LinearLayout {
                 Glide.with(getContext())
                         .load(previewData.getImages().get(0).getSource())
                         .into(image);
+            } else {
+                Glide.with(getContext()).load("clear it").into(image);
             }
             title.setText(previewData.getTitle().isEmpty() ? "Untitled" : previewData.getTitle());
             description.setText(previewData.getDescription().isEmpty() ? "No description!" : previewData.getDescription());
