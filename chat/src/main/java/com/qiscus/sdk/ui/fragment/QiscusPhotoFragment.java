805ab8b6440fe70c2b1bf63d1f175bcf30bbc46f
Zetra <zetra@mail.ugm.ac.id>
2017-06-15 09:30:15 +0700

    Clean up

--- chat/src/main/java/com/qiscus/sdk/ui/fragment/QiscusPhotoFragment.java
+++ chat/src/main/java/com/qiscus/sdk/ui/fragment/QiscusPhotoFragment.java
@@ -20,7 +20,6 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -71,7 +70,6 @@ public class QiscusPhotoFragment extends RxFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         resolveImageFile(savedInstanceState);
-        Log.d("ZETRA", "File: "+imageFile.getAbsolutePath());
         Glide.with(this).load(imageFile).diskCacheStrategy(DiskCacheStrategy.SOURCE).into(imageView);
     }
 
