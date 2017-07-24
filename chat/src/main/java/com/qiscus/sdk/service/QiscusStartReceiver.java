7e82979c8c69cc94a8b6dbf579686b91fc3d394a
Zetra <zetra@mail.ugm.ac.id>
2017-07-24 13:48:42 +0700

    Clean up

--- chat/src/main/java/com/qiscus/sdk/service/QiscusStartReceiver.java
+++ chat/src/main/java/com/qiscus/sdk/service/QiscusStartReceiver.java
@@ -19,7 +19,6 @@ package com.qiscus.sdk.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.qiscus.sdk.Qiscus;
 import com.qiscus.sdk.util.QiscusAndroidUtil;
