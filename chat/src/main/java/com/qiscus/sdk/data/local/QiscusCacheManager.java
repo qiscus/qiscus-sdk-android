483f600c431b4d82917ad6732e1bf275cade987c
Zetra <zetra@mail.ugm.ac.id>
2017-09-08 16:33:26 +0700

    Clear pending push notif key

--- chat/src/main/java/com/qiscus/sdk/data/local/QiscusCacheManager.java
+++ chat/src/main/java/com/qiscus/sdk/data/local/QiscusCacheManager.java
@@ -83,7 +83,7 @@ public enum QiscusCacheManager {
 
     public void clearMessageNotifItems(int roomId) {
         sharedPreferences.edit()
-                .putString("push_notif_message_" + roomId, "")
+                .remove("push_notif_message_" + roomId)
                 .apply();
     }
 
