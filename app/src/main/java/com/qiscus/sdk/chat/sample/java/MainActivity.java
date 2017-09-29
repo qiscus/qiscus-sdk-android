0244edaf70248aa9829724b655817dc78ab4a36d
Zetra <zetra@mail.ugm.ac.id>
2017-09-29 16:26:19 +0700

    Setup gradle for bintray

--- app/src/main/java/com/qiscus/sdk/chat/sample/java/MainActivity.java
+++ app/src/main/java/com/qiscus/sdk/chat/sample/java/MainActivity.java
@@ -20,11 +20,15 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 
 import com.qiscus.sdk.chat.core.Qiscus;
 import com.qiscus.sdk.chat.core.QiscusUseCaseFactory;
+import com.qiscus.sdk.chat.domain.interactor.Action;
 import com.qiscus.sdk.chat.domain.interactor.account.AuthenticateWithKey;
 import com.qiscus.sdk.chat.domain.interactor.room.GetRoomWithUserId;
+import com.qiscus.sdk.chat.domain.model.Account;
+import com.qiscus.sdk.chat.domain.model.Room;
 import com.qiscus.sdk.chat.sample.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -35,21 +39,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        findViewById(R.id.loginButton).setOnClickListener(view -> authenticate());
+        findViewById(R.id.loginButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticate();
+            }
+        });
 
-        findViewById(R.id.startChatButton).setOnClickListener(view -> openChatWith("rya.meyvriska244@gmail.com"));
+        findViewById(R.id.startChatButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openChatWith("rya.meyvriska244@gmail.com");
+            }
+        });
     }
 
     private void authenticate() {
         AuthenticateWithKey authenticate = useCaseFactory.authenticateWithKey();
 
         authenticate.execute(new AuthenticateWithKey.Params("zetra255@gmail.com", "12345678", "Zetra"),
-                account -> Log.d("ZETRA", "Login with " + account), Throwable::printStackTrace);
+                new Action<Account>() {
+                    @Override
+                    public void call(Account account) {
+                        Log.d("ZETRA", "Login with " + account);
+                    }
+                }, new Action<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        throwable.printStackTrace();
+                    }
+                });
     }
 
     private void openChatWith(String userId) {
         GetRoomWithUserId getRoom = useCaseFactory.getRoomWithUserId();
 
-        getRoom.execute(new GetRoomWithUserId.Params(userId), room -> Log.d("ZETRA", "Room: " + room));
+        getRoom.execute(new GetRoomWithUserId.Params(userId), new Action<Room>() {
+            @Override
+            public void call(Room room) {
+                Log.d("ZETRA", "Room: " + room);
+            }
+        });
     }
 }
