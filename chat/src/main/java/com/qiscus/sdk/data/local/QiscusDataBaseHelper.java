a1a7b7e7ddf5efaab6ab552518669364eb53e911
Zetra <zetra@mail.ugm.ac.id>
2017-10-16 14:44:17 +0700

    Clean up

--- chat/src/main/java/com/qiscus/sdk/data/local/QiscusDataBaseHelper.java
+++ chat/src/main/java/com/qiscus/sdk/data/local/QiscusDataBaseHelper.java
@@ -137,29 +137,6 @@ public class QiscusDataBaseHelper implements QiscusDataStore {
     }
 
     @Override
-    public QiscusChatRoom getChatRoomByUniqueId(String uniqueId) {
-        QiscusChatRoom qiscusChatRoom = null;
-        String query = String.format(
-                "SELECT * FROM %s WHERE %s = %s AND %s = 1",
-                QiscusDb.RoomTable.TABLE_NAME,
-                QiscusDb.RoomTable.COLUMN_DISTINCT_ID,
-                DatabaseUtils.sqlEscapeString(uniqueId),
-                QiscusDb.RoomTable.COLUMN_IS_GROUP
-        );
-        Cursor cursor = sqLiteDatabase.rawQuery(query, null);
-        if (cursor.moveToNext()) {
-            qiscusChatRoom = QiscusDb.RoomTable.parseCursor(cursor);
-            qiscusChatRoom.setMember(getRoomMembers(qiscusChatRoom.getId()));
-            QiscusComment latestComment = getLatestComment(qiscusChatRoom.getId());
-            if (latestComment != null) {
-                qiscusChatRoom.setLastComment(latestComment);
-            }
-        }
-        cursor.close();
-        return qiscusChatRoom;
-    }
-
-    @Override
     public QiscusChatRoom getChatRoom(String email, String distinctId) {
         String query = "SELECT * FROM "
                 + QiscusDb.RoomMemberTable.TABLE_NAME + " WHERE "
