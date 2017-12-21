648c678cfea112991ba2fa27f50fbc63c019c53a
Zetra <zetra@mail.ugm.ac.id>
2017-12-21 13:44:06 +0700

    Adding location adapter delegates

--- mobile/src/main/java/com/qiscus/sdk/chat/presentation/mobile/chatroom/ChatRoomActivity.kt
+++ mobile/src/main/java/com/qiscus/sdk/chat/presentation/mobile/chatroom/ChatRoomActivity.kt
@@ -83,6 +83,8 @@ class ChatRoomActivity : AppCompatActivity(), ListMessageContract.View, SendMess
                 .addDelegate(OpponentTextAdapterDelegate(this, this, this))
                 .addDelegate(ContactAdapterDelegate(this, this, this))
                 .addDelegate(OpponentContactAdapterDelegate(this, this, this))
+                .addDelegate(LocationAdapterDelegate(this, this, this))
+                .addDelegate(OpponentLocationAdapterDelegate(this, this, this))
                 .addDelegate(SystemEventAdapterDelegate(this))
 
         //Fallback adapter delegate
