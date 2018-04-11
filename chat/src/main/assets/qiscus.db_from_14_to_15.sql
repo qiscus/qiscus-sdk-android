c03dda61dc7cdbc9587817178782319b602d509f
Andhika Yuana <andhikayuana@gmail.com>
2018-04-11 11:22:46 +0700

    add index message

--- chat/src/main/assets/qiscus.db_from_14_to_15.sql
+++ chat/src/main/assets/qiscus.db_from_14_to_15.sql
@@ -2,5 +2,6 @@
  * migration db v14 to v15
  */
 CREATE INDEX comments_room_id_index ON comments (room_id);
+CREATE INDEX comments_message_index ON comments (message);
 CREATE INDEX room_members_distinct_id_index ON room_members (distinct_id);
 CREATE INDEX rooms_unique_id_index ON rooms (unique_id);
