3eb2cb6191512dd52914106e7c1484b37d6af084
Arief Nur Putranto <arief@qiscus.co>
2022-08-18 12:33:08 +0700

    update unit test

--- chat-core/src/test/java/com/qiscus/sdk/chat/core/util/BuildVersionUtilTest.java
+++ chat-core/src/test/java/com/qiscus/sdk/chat/core/util/BuildVersionUtilTest.java
@@ -1,30 +1,61 @@
 package com.qiscus.sdk.chat.core.util;
 
-import static org.junit.Assert.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import android.os.Build;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class BuildVersionUtilTest {
 
     @Test
     public void isOreoLower() {
-        assertTrue(BuildVersionUtil.isOreoLower());
+        if (BuildVersionUtil.isOreoLower()) {
+            assertTrue(Build.VERSION.SDK_INT < Build.VERSION_CODES.O);
+        }
     }
 
     @Test
     public void isOreoOrHigher() {
-        assertTrue(BuildVersionUtil.isOreoOrHigher());
+        if (BuildVersionUtil.isOreoOrHigher()) {
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.O);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.O_MR1);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.P);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.Q);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.R);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.S);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.S_V2);
+        }
     }
 
     @Test
     public void isNougatOrHigher() {
-        assertTrue(BuildVersionUtil.isNougatOrHigher());
+        if (BuildVersionUtil.isNougatOrHigher()) {
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.O);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.O_MR1);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.P);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.Q);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.R);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.S);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.S_V2);
+        }
     }
 
     @Test
     public void isAtLeastNMR1() {
-        assertTrue(BuildVersionUtil.isAtLeastNMR1());
+        if (BuildVersionUtil.isAtLeastNMR1()) {
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.O);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.O_MR1);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.P);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.Q);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.R);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.S);
+            assertEquals(Build.VERSION.SDK_INT, Build.VERSION_CODES.S_V2);
+        }
     }
 }
--- chat-core/src/test/java/com/qiscus/sdk/chat/core/util/QiscusNumberUtilTest.java
+++ chat-core/src/test/java/com/qiscus/sdk/chat/core/util/QiscusNumberUtilTest.java
@@ -1,21 +1,8 @@
 package com.qiscus.sdk.chat.core.util;
 
-
-import static net.bytebuddy.matcher.ElementMatchers.is;
-import static org.hamcrest.core.IsInstanceOf.instanceOf;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
-import net.bytebuddy.matcher.ElementMatcher;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
 
 public class QiscusNumberUtilTest {
     @Test
